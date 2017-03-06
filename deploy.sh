56234ad91c6dce2eb45120d59b0f4a5584c98d37
Bulat Lutfullin <lb6557@gmail.com>
2017-03-06 19:47:19 +0300

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker build -t kineticcookie/sample-node .
+docker push kineticcookie/sample-node
 
-ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+ssh deploy@35.187.18.155 << EOF
+docker pull kineticcookie/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi kineticcookie/sample-node:current || true
+docker tag kineticcookie/sample-node:latest kineticcookie/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 kineticcookie/sample-node:current
 EOF
