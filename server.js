015823b285d1699e8c571f5e0cb203dee1131c77
Bulat Lutfullin <lb6557@gmail.com>
2017-03-06 20:01:31 +0300

    trigger

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 
 express()
  .get("*", function(req, res) {
-	 res.send("Continuous integration and deployment lab finished");
+	res.send("Continuous integration and deployment lab finished");
  })
  .listen(80, function(err) {
     if (err) {
